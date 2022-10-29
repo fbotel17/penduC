7bdd68690b2287ab86a872de165e218c72c276f4
fbotel17 <faustbtl17@gmail.com>
2022-10-29 16:18:02 +0200

    validation des lettres, mais problème

--- BasePenduC-main/Projet-Pendu-Base/main.cpp
+++ BasePenduC-main/Projet-Pendu-Base/main.cpp
@@ -12,37 +12,90 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 	char lettre;
-	int nombreCoup;
+	int compteur;
+	int nombreCoup=0;
 	char mot[100];
-	piocherMot(mot);
+	char tablettre[100];
 
+	piocherMot(mot);
+	compteur = 0;
 	cout << "Bienvenue dans le Pendu !" << endl;
 	
-	cout << "mot secret : ";
+	cout << endl << "mot secret : ";
 	for (int i = 0; i < strlen(mot); i++)
 	{
 		cout << "*";
 	}
 
-	do {
-		cout << "Proposez une lettre : ";
-		cin >> lettre;
-		if (lettre >= 97 and lettre <= 122)
-		{
-			lettre = lettre - 32;
-		}
-		else if (lettre >= 65 and lettre <= 90)
-		{
-			lettre = lettre;
-		}
-		else
-		{
-			cout << "saisie incorrecte, retapez votre lettre : ";
-		}
-	}while(!(lettre >= 65 and lettre <= 90));
-	cout << lettre << endl;
-
+	cout << "Combien d'essais pour reussir ? : ";
+	cin >> nombreCoup;
 	
+	while (nombreCoup < strlen(mot))
+	{
+		cout << "Nombre d'essais insuffisant pour réussir, retapez un nombre d'essais ? : ";
+		cin >> nombreCoup;
+
+	}
+
+	for (int i = 0; i < strlen(mot); i++)
+	{
+		cout <<endl<< mot[i];
+	}
+
+	int j = 0;
+	int o = 0;
+	do
+	{
+		cout << "Il reste " << nombreCoup - j << " coups" << endl;
+		//saisie d'une lettre
+		do {
+			cout << endl << "Proposez une lettre : ";
+			cin >> lettre;
+			if (lettre >= 97 and lettre <= 122)
+			{
+				lettre = lettre - 32;
+			}
+			else if (lettre >= 65 and lettre <= 90)
+			{
+				lettre = lettre;
+			}
+			else
+			{
+				cout << "saisie incorrecte, retapez votre lettre";
+			}
+		} while (!(lettre >= 65 and lettre <= 90));
+
+
+
+		for (int i = 0; i < strlen(mot); i++)
+		{
+			
+			if (lettre == mot[i])
+			{
+				cout << mot[i];
+				tablettre[o] = lettre;
+				o++;
+			}
+			else if (tablettre[i] == mot[i])
+			{
+				cout << mot[i];
+			}
+			else
+			{
+				cout << "*";
+			}
+		
+			
+		}
+		cout << endl;
+		j++;
+	} while (nombreCoup != j);
+	//or compteur != strlen(mot)
+
+
+
+
+
 	//--------------------------------------------------
 	// Exemple d'utilisation du dictionnaire de mot : 
 	//piocherMot(mot);	// Pour le moment la fonction renvoi toujours
