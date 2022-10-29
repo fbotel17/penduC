d93034066fa07f1398a1c208cd534e87b2513eb8
fbotel17 <faustbtl17@gmail.com>
2022-10-29 17:59:32 +0200

    cmd bug

--- BasePenduC-main/Projet-Pendu-Base/main.cpp
+++ BasePenduC-main/Projet-Pendu-Base/main.cpp
@@ -12,117 +12,146 @@ using namespace std;
 int main(int argc, char ** argv)
 {
 	char lettre;
+	char modeJeu;
 	int compteur;
 	int nombreCoup=0;
 	char mot[100];
 	char tablettre[100];
 	int j = 0;
-
-	
-	piocherMot(mot);
-
-	for (int i = 0; i < strlen(mot); i++)
-	{
-		tablettre[i] = '*';
-	}
-	compteur = 0;
-	cout << "Bienvenue dans le Pendu !" << endl;
-	
-	cout << endl << "mot secret : ";
-	for (int i = 0; i < strlen(mot); i++)
-	{
-		cout << "*";
-	}
-
-	cout << endl << "Combien d'essais pour reussir ? : ";
-	cin >> nombreCoup;
-	while (nombreCoup < 0)
-	{
-		cout << "Nombre d'essais insuffisant pour réussir, retapez un nombre d'essais ? : ";
-		cin >> nombreCoup;
-
-	}
-	
-	for (int i = 0; i < strlen(mot); i++)
-	{
-		cout << mot[i];
-	}
+	char arret='a';
 
 	do
 	{
-		cout << "Il reste " << nombreCoup << " coups" << endl;
-		//saisie d'une lettre
-		do {
-			cout << endl << "Proposez une lettre : ";
-			cin >> lettre;
-			if (lettre >= 97 and lettre <= 122)
-			{
-				lettre = lettre - 32;
-			}
-			else if (lettre >= 65 and lettre <= 90)
-			{
-				lettre = lettre;
-			}
-			else
-			{
-				cout << "saisie incorrecte, retapez votre lettre";
-			}
-		} while (!(lettre >= 65 and lettre <= 90));
-
-
+		system("CLS");
 
 		for (int i = 0; i < strlen(mot); i++)
 		{
-			
-			if (lettre == mot[i])
-			{
-				
-				tablettre[i] = lettre;
-				compteur++;
-				nombreCoup++;
-			}
-			else if(tablettre[i] != '*')
-			{
-				tablettre[i] = tablettre[i];
-			}
-			else 
-			{
-				tablettre[i]='*';
-			}
-			
+			tablettre[i] = '*';
 		}
+		compteur = 0;
+		cout << "Bienvenue dans le Pendu !" << endl;
 		
-		
-		for (int y = 0; y < strlen(mot); y++)
+		do
 		{
-			cout << tablettre[y];
-		}
-
-
-		cout << endl;
+			cout << "Mode solo - tapez 1" << endl;
+			cout << "Mode duo - tapez 2" << endl;
+			cin >> modeJeu;
+			if (modeJeu != '1' and modeJeu != '2')
+			{
+				cout << "Saisie incorrecte" << endl;
+			}
+		} while (modeJeu != '1' and modeJeu != '2');
 		
-		if (compteur == strlen(mot))
+
+		if (modeJeu == '1')
 		{
-			break;
+			piocherMot(mot);
 		}
-		nombreCoup--;
-		
-	} while (nombreCoup != 0);
+		else
+		{
+			cout << "Joueur 1 entre ton mot, ne pas montrer a Joueur 2 :";
+				cin >> mot;
+		}
 
-	if (compteur == strlen(mot))
-	{
-		cout << "VICTOIRE !" << endl;
-	}
-	else
-	{
-		cout << "C'est perdu ! le mot etait : ";
+
+		cout << endl << "mot secret : ";
+		for (int i = 0; i < strlen(mot); i++)
+		{
+			cout << "*";
+		}
+
+		cout << endl << "Combien d'essais pour reussir ? : ";
+		cin >> nombreCoup;
+		while (nombreCoup < 0)
+		{
+			cout << "Nombre d'essais insuffisant pour réussir, retapez un nombre d'essais ? : ";
+			cin >> nombreCoup;
+
+		}
+
 		for (int i = 0; i < strlen(mot); i++)
 		{
 			cout << mot[i];
 		}
-	}
+
+		do
+		{
+			cout << endl << "Il reste " << nombreCoup << " coups" << endl;
+			//saisie d'une lettre
+			do {
+				cout << endl << "Proposez une lettre : ";
+				cin >> lettre;
+				if (lettre >= 97 and lettre <= 122)
+				{
+					lettre = lettre - 32;
+				}
+				else if (lettre >= 65 and lettre <= 90)
+				{
+					lettre = lettre;
+				}
+				else
+				{
+					cout << "saisie incorrecte, retapez votre lettre";
+				}
+			} while (!(lettre >= 65 and lettre <= 90));
 
 
+
+			for (int i = 0; i < strlen(mot); i++)
+			{
+
+				if (lettre == mot[i])
+				{
+
+					tablettre[i] = lettre;
+					compteur++;
+					nombreCoup++;
+				}
+				else if (tablettre[i] != '*')
+				{
+					tablettre[i] = tablettre[i];
+				}
+				else
+				{
+					tablettre[i] = '*';
+				}
+
+			}
+
+
+			for (int y = 0; y < strlen(mot); y++)
+			{
+				cout << tablettre[y];
+			}
+
+
+			cout << endl;
+
+			if (compteur == strlen(mot))
+			{
+				break;
+			}
+			nombreCoup--;
+
+		} while (nombreCoup != 0);
+
+		if (compteur == strlen(mot))
+		{
+			cout << "VICTOIRE !" << endl;
+		}
+		else
+		{
+			cout << "C'est perdu ! le mot etait : ";
+			for (int i = 0; i < strlen(mot); i++)
+			{
+				cout << mot[i];
+			}
+		}
+
+		cout <<endl<< "Une partie va se relancer, si vous ne souhaitez pas rejouer ecrivez 0, sinon appuyez sur une touche pour rejouer" << endl;
+		cin>>arret;
+
+	}while (arret != '0');
 
 
 	//--------------------------------------------------
