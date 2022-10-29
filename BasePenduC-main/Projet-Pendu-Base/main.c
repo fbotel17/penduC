53a64ec4d4014ab394628fd7b7acdf37e50f1427
fbotel17 <faustbtl17@gmail.com>
2022-10-29 12:32:38 +0200

    test

---
+++ BasePenduC-main/Projet-Pendu-Base/main.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "dico.h"	// Pour utiliser le dictionnaire de mots
+
+int main(int argc, char ** argv)
+{
+	//--------------------------------------------------
+	// Exemple d'utilisation du dictionnaire de mot : 
+	char mot[100];
+	piocherMot(mot);	// Pour le moment la fonction renvoi toujours
+						// le même mot. Vous devez implémenter la fonction
+						// int nombreAleatoire(int nombreMax) dans le fichier
+						// dico.c pour que cela fonctionne correctement.
+	printf("Pioches : %s", mot);
+	//--------------------------------------------------
+
+	return 0;
+}
